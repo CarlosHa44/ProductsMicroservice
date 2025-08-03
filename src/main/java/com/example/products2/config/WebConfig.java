70ccd42a9a46315f665348748098ef30759ec19a
CarlosHa44 <carlosha176@gmail.com>
2025-08-03 04:59:58 -0500

    Fix: Web Config

--- src/main/java/com/example/products2/config/WebConfig.java
+++ src/main/java/com/example/products2/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8085")
+                        .allowedOrigins("http://localhost:8085","http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                         .allowedHeaders("*");
             }
