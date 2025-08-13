a57a44b8522af4b48d233ddead795c72ea05a7a0
CarlosHa44 <carlosha176@gmail.com>
2025-08-13 12:48:29 -0500

    fix:url variable

--- src/main/java/com/example/products2/client/CategoryClient.java
+++ src/main/java/com/example/products2/client/CategoryClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "category", url = "c-api-categories:8004")
+@FeignClient(name = "category", url = "${service.categories.url}")
 public interface CategoryClient {
     @GetMapping("/api/categories/{id}")
     CategoryDTO getCategory(@PathVariable("id") Long id);
